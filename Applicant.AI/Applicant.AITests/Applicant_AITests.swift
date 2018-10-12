a854ec22e88f312fd5553925b3040cf2dee7147e
Troy Stacer <trstacer@umich.edu>
2018-10-12 15:53:26 -0400

    first commit of tabbed application

---
+++ Applicant.AI/Applicant.AITests/Applicant_AITests.swift
@@ -0,0 +1,34 @@
+//
+//  Applicant_AITests.swift
+//  Applicant.AITests
+//
+//  Created by Troy Stacer on 10/12/18.
+//  Copyright © 2018 Harbautics. All rights reserved.
+//
+
+import XCTest
+@testable import Applicant_AI
+
+class Applicant_AITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
