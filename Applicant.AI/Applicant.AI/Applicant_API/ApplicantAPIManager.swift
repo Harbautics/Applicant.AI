e4c91c4b27800972737ed75bf76b24f4069d2276
Jordan Wolff <jbwolff@umich.edu>
2018-10-30 20:09:03 -0400

    convenience init from JSON for organization, API function to make the call and load the organizations

--- Applicant.AI/Applicant.AI/Applicant_API/ApplicantAPIManager.swift
+++ Applicant.AI/Applicant.AI/Applicant_API/ApplicantAPIManager.swift
@@ -12,18 +12,22 @@ import CoreLocation
 public class ApplicantAPIManager {
     
     //URLs for APIs
-    private struct APIURLS {
-        
+    private struct APIURLs {
+        static let getOrganizations = URL(string: "https://applicant.ai/api/get-all-organizations")!
     }
     
+    // A generic fetch that gets JSON and calls the completion handler
     private class func fetch(url: URL, completionHandler: @escaping ((JSON?) -> Void)){
+        // use a separate thread
         DispatchQueue.global(qos: .default).async {
-            
+            // this is the fetch!
             let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                // if we get an error
                 if error != nil {
                     print(error!)
                     completionHandler(nil)
                 } else {
+                    // got data, call completion handler
                     if data != nil {
                         let json = JSON(data: data!)
                         completionHandler(json)
@@ -37,4 +41,22 @@ public class ApplicantAPIManager {
     }
     
     
+    // Get Organizations from the API
+    public class func getOrganizations(completionHandler: @escaping (([Organization]) -> Void )) {
+        
+        var organizations = [Organization]()
+        
+        // get dining hall data
+        let url = APIURLs.getOrganizations
+        fetch(url: url) { (json) in
+            if let organizationsJSON = json?.array {
+                organizations = organizationsJSON.map { Organization(json: $0)! }
+            }
+            
+            // Make sure we're back on the main thread
+            DispatchQueue.main.async {
+                completionHandler(organizations)
+            }
+        }
+    }
 }
--- Applicant.AI/Applicant.AI/Applicant_API/Organization.swift
+++ Applicant.AI/Applicant.AI/Applicant_API/Organization.swift
@@ -46,6 +46,42 @@ public class Organization: NSObject {
         super.init()
     }
     
+    convenience init?(json: JSON) {
+        // if we can pull information from JSON Response
+        if let name_JSON = json["name"].string,
+            let id_JSON = json["id"].string,
+            let members_JSON = json["members"].array,
+            let postings_JSON = json["postings"].array {
+            
+            // Go through the array of members, create and append member to the members array
+            var members_array = [Member]()
+            for member in members_JSON {
+                members_array.append(Member(id_in: member["id"].string!, name_in: member["name"].string!))
+            }
+            // Go through the array of postings, create and append posting to the posting array
+            var postings_array = [Posting]()
+            for posting in postings_JSON {
+                
+                // go through the questions and applicants, create and append
+                var questions_array = [Question]()
+                var applicants_array = [Applicant]()
+                for question in posting["questions"].array! {
+                    questions_array.append(Question(description_in: question["description"].string!, question_in: question["question"].string!, applicant_answer_in: question["answer"].string!))
+                }
+                for applicant in posting["applicants"].array! {
+                    applicants_array.append(Applicant(id_in: applicant["id"].string!, name_in: applicant["name"].string!))
+                }
+                
+                postings_array.append(Posting(name_in: posting["name"].string!, id_in: posting["id"].string!, status_in: posting["status"].string!, job_desc_in: posting["job-description"].string!, questions_in: questions_array, applicants_in: applicants_array))
+            }
+            
+            self.init(name: name_JSON, id: id_JSON, members: members_array, postings: postings_array)
+        }
+        else {
+            return nil
+        }
+    }
+    
     // Debeugging Description
     public override var description: String {
         return "\(id): \(name)"
