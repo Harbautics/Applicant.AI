cc6879e8f3868e379385b60a4646733486d43321
Jordan Wolff <jbwolff@umich.edu>
2018-11-22 01:10:37 -0600

    Major UI stuff

---
+++ Applicant.AI/Applicant.AI/CELLS/Organization_TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Organization_TableViewCell.swift
+//  Applicant.AI
+//
+//  Created by Jordan Wolff on 11/21/18.
+//  Copyright © 2018 Harbautics. All rights reserved.
+//
+
+import UIKit
+
+class Organization_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
